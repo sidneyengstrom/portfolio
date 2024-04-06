f487b7dbcbd8f08f9447c17e4088016e19c2e5a4
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-04-05 22:06:16 -0700

    Adds name, title, info to landing section

---
+++ src/components/Landing.jsx
@@ -0,0 +1,10 @@
+import LandingInfo from './LandingInfo';
+
+function Landing() {
+    return (
+        <section id="landing">
+            <LandingInfo />
+        </section>
+    )
+}
+export default Landing
