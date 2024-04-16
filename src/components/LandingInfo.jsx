e69030f267ec484221607f4162c05c77461188b7
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-04-16 12:25:37 -0700

    Adds typewriter effect to landing info

--- src/components/LandingInfo.jsx
+++ src/components/LandingInfo.jsx
@@ -1,4 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
 function LandingInfo () {
+    const [displayText, setDisplayText] = useState('');
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [isTyping, setIsTyping] = useState(true);
+    
+    const Typewriter = ({ text }) => {
+      useEffect(() => {
+        if (currentIndex < text.length && isTyping) {
+          const timeout = setTimeout(() => {
+            setDisplayText((prevText) => prevText + text[currentIndex]);
+            setCurrentIndex((prevIndex) => prevIndex + 1);
+          }, 20);
+    
+          return () => clearTimeout(timeout);
+        } else {
+          setIsTyping(false);
+        }
+      }, [currentIndex, text, isTyping]);
+    
+      return <span>{displayText}</span>;
+    };
+      
+    const textToType = "I am a developer based in Vancouver, BC with a passion for creating clean, manageable code behind appealing and accessible user interfaces.";
+
     return (
         <div className="landing-info">
             <div className="name">
@@ -6,11 +31,11 @@ function LandingInfo () {
                 <h1 className="accent-text">Engstrom</h1>
             </div>
             <div className="job-title">
-                <h2>/ Front-End Web Developer</h2>
+                <h2>/ Front-End Developer</h2>
                 <h2>/ Web Designer</h2>
             </div>
-            <div className="lorem self-desc">
-                <p>Lorem ipsum dolor sit <span className="accent-text">amet, consectetur</span> adipiscing elit. Mauris sagittis scelerisque libero. <span className="accent-text">Morbi a ex faucibus</span> leo tincidunt viverra quis nec nisi.</p>
+            <div className="self-desc">
+                <Typewriter text={textToType} />
             </div>
             <div className="cta">
                 <button className="primary cta">See my work</button>
