fbb1c2ff80e4c1039871c8a0349df5c78156ce47
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-04-16 01:23:29 -0700

    Renames landing info classes for clarity

--- src/components/LandingInfo.jsx
+++ src/components/LandingInfo.jsx
@@ -1,18 +1,18 @@
 function LandingInfo () {
     return (
-        <div className="landing-info-wrap">
-            <div className="name-wrap">
+        <div className="landing-info">
+            <div className="name">
                 <h1>Sidney</h1>
                 <h1 className="accent-text">Engstrom</h1>
             </div>
-            <div className="job-title-wrap">
+            <div className="job-title">
                 <h2>/ Front-End Web Developer</h2>
                 <h2>/ Web Designer</h2>
             </div>
-            <div className="lorem self-desc-wrap">
+            <div className="lorem self-desc">
                 <p>Lorem ipsum dolor sit <span className="accent-text">amet, consectetur</span> adipiscing elit. Mauris sagittis scelerisque libero. <span className="accent-text">Morbi a ex faucibus</span> leo tincidunt viverra quis nec nisi.</p>
             </div>
-            <div className="cta-wrap">
+            <div className="cta">
                 <button className="primary cta">See my work</button>
                 <button className="secondary cta">Learn about me</button>
             </div>
