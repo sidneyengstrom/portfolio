7fcd3efae1f3bb80631ffde635c13fff8afd1e64
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-04-17 14:01:06 -0700

    Imports project data into featured work section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Landing from './components/Landing';
 import Navigation from './components/Navigation';
+import FeaturedWork from './components/FeaturedWork';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navigation />
       <Landing />
-    </>
+      <FeaturedWork />
+    </Router>
   )
 }
 
