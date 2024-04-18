db373557e56c0bb5d6b74723810fafa79698bbcd
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-04-17 21:58:48 -0700

    Switches left and right swipe effects on project cards

--- src/components/FeaturedWork.jsx
+++ src/components/FeaturedWork.jsx
@@ -33,20 +33,20 @@ function FeaturedWork () {
             <div className="project-wrapper">
             {projects.map((project, index) => (
                 project.feature ? (
-                    <div className={`project ${index % 2 === 0 ? 'right-swipe' : 'left-swipe'}`} key={index} ref={projectRefs.current[index] || (projectRefs.current[index] = useRef())}>
+                    <div className={`project ${index % 2 === 0 ? 'left-swipe' : 'right-swipe'}`} key={index} ref={projectRefs.current[index] || (projectRefs.current[index] = useRef())}>
                         <img src={`/src/assets/images/${project.thumbsrc}`} alt={project.thumbalt} />
                         <h3>{project.title}</h3>
                         <p>{project.excerpt}</p>
-                        <Link to={`/project/${project.slug}`}>
-                            <div className="learn-more">
+                        <div className="learn-more">
+                            <Link to={`/project/${project.slug}`}>
                                 <p>Learn More</p>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="icon arrow" width="19" height="19" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                     <path d="M17 7l-10 10" />
                                     <path d="M8 7l9 0l0 9" />
                                 </svg>
-                            </div>
-                        </Link>
+                            </Link>
+                        </div>
                         <div className="tech-used">
                             {project.tech.map((tech, index) => (
                                 <p className='tech' key={index}>{tech}</p>
