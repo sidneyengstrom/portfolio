c79d87ed2e5207519af5bf0172ef536d53a684d6
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-04-18 16:19:08 -0700

    Adds and adjusts styling for project detail links

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -15,6 +15,27 @@ function ProjectDetails() {
   return (
     <div className="project-page-wrapper">
       <FeaturedCarousel />
+      <div className="project-links">
+        <p>{project.repo ? <a href={project.repoLink}>
+            GitHub
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-external-link" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+              <path d="M11 13l9 -9" />
+              <path d="M15 4h5v5" />
+            </svg>
+        </a> : 'GitHub Repository currently private.'}</p>
+
+        <p><a href={project.liveSite}>
+          Live Site
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-external-link" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+              <path d="M11 13l9 -9" />
+              <path d="M15 4h5v5" />
+          </svg>
+          </a></p>
+      </div>
     </div>
   );
 }
