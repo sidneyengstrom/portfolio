c697a86a3980387e3beab706aaec025426912cfa
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-04-18 15:14:00 -0700

    Adds home page component

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Landing from './Landing';
+import FeaturedWork from './FeaturedWork';
+
+function Home() {
+    return (
+        <>
+            <Landing />
+            <FeaturedWork />
+        </>
+    )
+  }
+  
+  export default Home
