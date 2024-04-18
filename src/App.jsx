4931c9f0571c4dcd65aae8d54933f0d73af93725
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-04-18 15:13:02 -0700

    Routes home and project details pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Landing from './components/Landing';
+import Home from './components/Home';
 import Navigation from './components/Navigation';
-import FeaturedWork from './components/FeaturedWork';
-import { BrowserRouter as Router } from 'react-router-dom';
+import ProjectDetails from './components/ProjectDetails';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      
-      <Landing />
-      <FeaturedWork />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/project/:slug" element={<ProjectDetails />}></Route>
+      </Routes>
       <Navigation />
     </Router>
   )
