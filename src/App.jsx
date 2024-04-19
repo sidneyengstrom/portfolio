38434f52ad1fcd88f53bd995b8a28256f296c70d
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-04-19 01:04:42 -0700

    Creates and routes all projects page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from './components/Home';
 import Navigation from './components/Navigation';
 import ProjectDetails from './components/ProjectDetails';
+import AllProjects from './components/AllProjects';
 // import AllProjects from './components/AllProjects';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/project/:slug" element={<ProjectDetails />}></Route>
-        {/* <Route path="projects" element={<AllProjects />}></Route> */}
+        <Route path="/projects" element={<AllProjects />}></Route>
       </Routes>
       <Navigation />
     </Router>
