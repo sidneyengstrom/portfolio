855ff8b2c68647c1ca2e47ad4acb8e580c54a9e9
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-05-01 22:25:13 -0700

    Adds, animates, styles computer art in landing section

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,12 @@
 import LandingInfo from './LandingInfo';
 import Socials from './Socials';
+import LandingDecoration from './LandingDecoration';
 
 function Landing({ darkMode, toggleDarkMode }) {
     return (
         <section id="landing">
             <LandingInfo />
+            <LandingDecoration />
             <Socials darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
         </section>
     )
---
+++ src/components/LandingDecoration.jsx
@@ -0,0 +1,11 @@
+import Computer from './Computer';
+function LandingDecoration() {
+    return (
+        <div class="art-wrapper">
+            <Computer />
+            <Window />
+            <DateTime />
+        </div>
+    )
+}
+export default LandingDecoration
