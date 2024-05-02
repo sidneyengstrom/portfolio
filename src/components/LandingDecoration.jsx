a681e20396d02887f7cfdfa4c0c079f702da1e17
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-05-01 22:41:21 -0700

    Adds accessibility clarification to landing decor

--- src/components/LandingDecoration.jsx
+++ src/components/LandingDecoration.jsx
@@ -1,7 +1,10 @@
 import Computer from './Computer';
+import Window from './Window';
+import DateTime from './DateTime';
+
 function LandingDecoration() {
     return (
-        <div class="art-wrapper">
+        <div class="art-wrapper" role="presentation" aria-roledescription="presentation">
             <Computer />
             <Window />
             <DateTime />
