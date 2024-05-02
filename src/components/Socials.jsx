220b6071f3be0dd17e30160144f8d4d69a928cc3
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-05-02 03:23:29 -0700

    Implements l+d transitions and icon changing

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -17,12 +17,28 @@ function Socials ({ darkMode, toggleDarkMode }) {
         <div className="socials">
             <div className="social-bar"></div>
             <div className="social-icon dark-mode" onClick={toggleDarkMode}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FFFFFF" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
-                    <path d="M17 4a2 2 0 0 0 2 2a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2a2 2 0 0 0 2 -2" />
-                    <path d="M19 11h2m-1 -1v2" />
-                </svg>
+                {darkMode ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FFFFFF" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+                        <path d="M17 4a2 2 0 0 0 2 2a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2a2 2 0 0 0 2 -2" />
+                        <path d="M19 11h2m-1 -1v2" />
+                    </svg>
+                ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun-low" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+                        <path d="M4 12h.01" />
+                        <path d="M12 4v.01" />
+                        <path d="M20 12h.01" />
+                        <path d="M12 20v.01" />
+                        <path d="M6.31 6.31l-.01 -.01" />
+                        <path d="M17.71 6.31l-.01 -.01" />
+                        <path d="M17.7 17.7l.01 .01" />
+                        <path d="M6.3 17.7l.01 .01" />
+                    </svg>
+                )}
+                
             </div>
 
             <div className="social-icon github">
