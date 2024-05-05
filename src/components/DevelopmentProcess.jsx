7e53926bb35cd97c9f45d1475d72bec80f4f7d7c
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-05-04 20:15:54 -0700

    Creates development process accordion tab

---
+++ src/components/DevelopmentProcess.jsx
@@ -0,0 +1,27 @@
+function DevelopmentProcess({ project }) {
+    return (
+        <div className='panel'>
+            <div className='panel'>
+                {project.process.development.text.map((developmentParagraph, index) => (
+                    <p key={index}>{developmentParagraph}</p>
+                ))}
+                    
+                {project.process.development.featuredImages && (
+                    <div className={`featured-image-wrapper grid-col-${project.process.development.featuredImages.length +(project.process.development.videos ? project.process.development.videos.length : 0)}`}>
+                        {project.process.development.featuredImages.map((featuredImage, index) => (
+                            <img src={`/src/assets/images/${featuredImage}`} key={index} alt={`Featured Image ${index}`} />
+                        ))}
+                        {project.process.development.videos && project.process.development.videos.map((video, index) => (
+                            <video key={index}>
+                                <source src={`/src/assets/videos/process/${video}`} type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default DevelopmentProcess;
